df8124b8e64f197d0947454d883d36484650e8f0
angus51406 <68845868+angus51406@users.noreply.github.com>
2020-07-30 14:26:17 +0800

    Add files via upload

---
+++ day4-2.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 10:19:10 2020
+
+@author: user
+"""
+
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import random
+
+x,y,z = mc.player.getTilePos()
+for i in range(20):
+    r = random.randrange(1,7)
+    if r == 1:
+        mc.setBlocks(x,y,z,x,y,z+4,46)
+        z = z+4
+    if r == 2:
+        mc.setBlocks(x,y,z,x,y,z-4,137)
+        z = z-4
+    if r == 3:
+        mc.setBlocks(x,y,z,x+4,y,z,165)
+        x = x+4
+    if r == 4:
+        mc.setBlocks(x,y,z,x-4,y,z,426)
+        z = z-4
+    if r == 5:
+        mc.setBlocks(x,y,z,x,y+4,z,138)
+        y = y+4
+    if r == 6:
+        mc.setBlocks(x,y,z,x,y-4,z,61)
+        y = y-4    
